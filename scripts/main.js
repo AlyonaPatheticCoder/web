a4d97b23033b473c4237cc81b70245c4a7e73130
Alena Ushakova <pottermania2016@gmail.com>
2024-12-01 00:28:09 +0300

    commit_2: lab-4; gallery.html, me.html, main.js added

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const currentLocation = location.href;
+    const menuItem = document.querySelectorAll('.navigation__link');
+    const menuLenght = menuItem.length
+    for (let i = 0; i<menuLenght; i++) {
+        if (menuItem[i].href === currentLocation) {
+            menuItem[i].className = "active navigation__link";
+        }
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const startTime = performance.now();
+    window.addEventListener("load", function() {
+        const endTime = performance.now();
+        const loadTime = (endTime - startTime).toFixed(1);
+        const loadTimeElement = document.getElementById("load-time");
+        loadTimeElement.textContent = `Время загрузки страницы: ${loadTime} мс`;
+    });
+});
